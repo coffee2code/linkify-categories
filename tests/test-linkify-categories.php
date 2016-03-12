8f4049be212358ede54ac97828e5d63e811779cb
Scott Reilly <scott@coffee2code.com>
2016-03-12 13:12:28 -0700

    Release v2.2:
    * Change: Update widget to 004:
        * Add `register_widget()` and change to calling it when hooking 'admin_init'
        * Add `version()` to return the widget's version
        * Reformat config array
        * Discontinue use of old-style constructor
        * Add inline docs for class variables
        * Late-escape attribute values
        * Reorder some conditional expressions
    * Change: Explicitly declare methods in unit tests as public or protected
    * Change: Fix and simplify unit tests. Add tests for widget
    * New: Add 'Text Domain' to plugin header
    * New: Add LICENSE file
    * New: Add empty index.php to prevent files from being listed if web server has enabled directory listings
    * Change: Use DIRECTORY_SEPARATOR in place of '/' when requiring widget file
    * Change: Note compatibility through WP 4.4+
    * Change: Update copyright date (2016)

--- tests/test-linkify-categories.php
+++ tests/test-linkify-categories.php
@@ -1,40 +1,50 @@
 <?php
 
+defined( 'ABSPATH' ) or die();
+
 class Linkify_Categories_Test extends WP_UnitTestCase {
 
 	private static $cat_ids = array();
 
-	function setUp() {
+	public function setUp() {
 		parent::setUp();
 		$this->cat_ids = $this->factory->category->create_many( 5 );
 	}
 
 
-	/*
-	 *
-	 * HELPER FUNCTIONS
-	 *
-	 */
+	//
+	//
+	// HELPER FUNCTIONS
+	//
+	//
 
 
-	function get_slug( $cat_id ) {
+	protected function get_slug( $cat_id ) {
 		return get_category( $cat_id )->slug;
 	}
 
-	function expected_output( $count, $lowest_id, $between = ', ', $cat_num = 1 ) {
+	/**
+	 * Returns the expected output.
+	 *
+	 * @param int    $count     The number of categories to list.
+	 * @param int    $cat_index Optional. The index into the $cat_ids array to start at. Default 0.
+	 * @param string $between   Optional. The string to appear between categories. Default ', '.
+	 * @param int    $cat_num   Optional. The category number. Default 1.
+	 * @return string
+	 */
+	protected function expected_output( $count, $cat_index = 0, $between = ', ', $cat_num = 1 ) {
 		$str = '';
-		$j = $lowest_id;
-		for ( $n = 1, $i = $cat_num; $n <= $count; $n++, $i++ ) {
+		for ( $n = 1; $n <= $count; $n++, $cat_index++ ) {
 			if ( ! empty( $str ) ) {
 				$str .= $between;
 			}
-			$str .= '<a href="http://example.org/?cat=' . $j . '" title="View all posts in Term ' . $i . '">Term ' . $i . '</a>';
-			$j++;
+			$cat = get_category( $this->cat_ids[ $cat_index ] );
+			$str .= '<a href="http://example.org/?cat=' . $cat->term_id . '" title="View all posts in ' . $cat->name . '">' . $cat->name . '</a>';
 		}
 		return $str;
 	}
 
-	function get_results( $args, $direct_call = true, $use_deprecated = false ) {
+	protected function get_results( $args, $direct_call = true, $use_deprecated = false ) {
 		ob_start();
 
 		$function = $use_deprecated ? 'linkify_categories' : 'c2c_linkify_categories';
@@ -51,78 +61,94 @@ class Linkify_Categories_Test extends WP_UnitTestCase {
 	}
 
 
-	/*
-	 *
-	 * TESTS
-	 *
-	 */
+	//
+	//
+	// TESTS
+	//
+	//
 
 
-	function test_single_id() {
-		$this->assertEquals( $this->expected_output( 1, $this->cat_ids[0] ), $this->get_results( array( $this->cat_ids[0] ) ) );
-		$this->assertEquals( $this->expected_output( 1, $this->cat_ids[0] ), $this->get_results( array( $this->cat_ids[0], false ) ) );
+	public function test_widget_class_name() {
+		$this->assertTrue( class_exists( 'c2c_LinkifyCategoriesWidget' ) );
 	}
 
-	function test_array_of_ids() {
-		$this->assertEquals( $this->expected_output( 5, $this->cat_ids[0] ), $this->get_results( array( $this->cat_ids ) ) );
-		$this->assertEquals( $this->expected_output( 5, $this->cat_ids[0] ), $this->get_results( array( $this->cat_ids ), false ) );
+	public function test_widget_version() {
+		$this->assertEquals( '004', c2c_LinkifyCategoriesWidget::version() );
 	}
 
-	function test_single_slug() {
+	public function test_widget_hooks_widgets_init() {
+		$this->assertEquals( 10, has_filter( 'widgets_init', array( 'c2c_LinkifyCategoriesWidget', 'register_widget' ) ) );
+	}
+
+	public function test_widget_made_available() {
+		$this->assertContains( 'c2c_LinkifyCategoriesWidget', array_keys( $GLOBALS['wp_widget_factory']->widgets ) );
+	}
+
+	public function test_single_id() {
+		$this->assertEquals( $this->expected_output( 1 ), $this->get_results( array( $this->cat_ids[0] ) ) );
+		$this->assertEquals( $this->expected_output( 1 ), $this->get_results( array( $this->cat_ids[0], false ) ) );
+	}
+
+	public function test_array_of_ids() {
+		$this->assertEquals( $this->expected_output( 5 ), $this->get_results( array( $this->cat_ids ) ) );
+		$this->assertEquals( $this->expected_output( 5 ), $this->get_results( array( $this->cat_ids ), false ) );
+	}
+
+	public function test_single_slug() {
 		$cat = get_category( $this->cat_ids[0] );
-		$this->assertEquals( $this->expected_output( 1, $cat->term_id ), $this->get_results( array( $cat->slug ) ) );
+		$this->assertEquals( $this->expected_output( 1 ), $this->get_results( array( $cat->slug ) ) );
 	}
 
-	function test_array_of_slugs() {
+	public function test_array_of_slugs() {
 		$cat_slugs = array_map( array( $this, 'get_slug' ), $this->cat_ids );
-		$this->assertEquals( $this->expected_output( 5, $this->cat_ids[0] ), $this->get_results( array( $cat_slugs ) ) );
-		$this->assertEquals( $this->expected_output( 5, $this->cat_ids[0] ), $this->get_results( array( $cat_slugs ), false ) );
+		$this->assertEquals( $this->expected_output( 5 ), $this->get_results( array( $cat_slugs ) ) );
+		$this->assertEquals( $this->expected_output( 5 ), $this->get_results( array( $cat_slugs ), false ) );
 	}
 
-	function test_all_empty_categories() {
+	public function test_all_empty_categories() {
 		$this->assertEmpty( $this->get_results( array( '' ) ) );
 		$this->assertEmpty( $this->get_results( array( array() ) ) );
 		$this->assertEmpty( $this->get_results( array( array( array(), '' ) ) ) );
 	}
 
-	function test_an_empty_category() {
+	public function test_an_empty_category() {
 		$cat_ids = array_merge( array( '' ), $this->cat_ids );
-		$this->assertEquals( $this->expected_output( 5, $this->cat_ids[0] ), $this->get_results( array( $cat_ids ) ) );
-		$this->assertEquals( $this->expected_output( 5, $this->cat_ids[0] ), $this->get_results( array( $cat_ids ), false ) );
+		$this->assertEquals( $this->expected_output( 5 ), $this->get_results( array( $cat_ids ) ) );
+		$this->assertEquals( $this->expected_output( 5 ), $this->get_results( array( $cat_ids ), false ) );
 	}
 
-	function test_all_invalid_categories() {
+	public function test_all_invalid_categories() {
 		$this->assertEmpty( $this->get_results( array( 99999999 ) ) );
 		$this->assertEmpty( $this->get_results( array( 'not-a-category' ) ) );
 		$this->assertEmpty( $this->get_results( array( 'not-a-category' ), false ) );
 	}
 
-	function test_an_invalid_category() {
+	public function test_an_invalid_category() {
 		$cat_ids = array_merge( array( 99999999 ), $this->cat_ids );
-		$this->assertEquals( $this->expected_output( 5, $this->cat_ids[0] ), $this->get_results( array( $cat_ids ) ) );
-		$this->assertEquals( $this->expected_output( 5, $this->cat_ids[0] ), $this->get_results( array( $cat_ids ), false ) );
+		$this->assertEquals( $this->expected_output( 5 ), $this->get_results( array( $cat_ids ) ) );
+		$this->assertEquals( $this->expected_output( 5 ), $this->get_results( array( $cat_ids ), false ) );
 	}
 
-	function test_arguments_before_and_after() {
-		$expected = '<div>' . $this->expected_output( 5, $this->cat_ids[0] ) . '</div>';
+	public function test_arguments_before_and_after() {
+		$expected = '<div>' . $this->expected_output( 5 ) . '</div>';
 		$this->assertEquals( $expected, $this->get_results( array( $this->cat_ids, '<div>', '</div>' ) ) );
 		$this->assertEquals( $expected, $this->get_results( array( $this->cat_ids, '<div>', '</div>' ), false ) );
 	}
 
-	function test_argument_between() {
-		$expected = '<ul><li>' . $this->expected_output( 5, $this->cat_ids[0], '</li><li>' ) . '</li></ul>';
+	public function test_argument_between() {
+		$expected = '<ul><li>' . $this->expected_output( 5, 0, '</li><li>' ) . '</li></ul>';
 		$this->assertEquals( $expected, $this->get_results( array( $this->cat_ids, '<ul><li>', '</li></ul>', '</li><li>' ) ) );
 		$this->assertEquals( $expected, $this->get_results( array( $this->cat_ids, '<ul><li>', '</li></ul>', '</li><li>' ), false ) );
 	}
 
-	function test_argument_before_last() {
+	public function test_argument_before_last() {
 		$before_last = ', and ';
-		$expected = $this->expected_output( 4, $this->cat_ids[0] ) . $before_last . $this->expected_output( 1, $this->cat_ids[4], ', ', 5 );
+		$expected = $this->expected_output( 4 ) . $before_last . $this->expected_output( 1, 4, ', ', 5 );
 		$this->assertEquals( $expected, $this->get_results( array( $this->cat_ids, '', '', ', ', $before_last ) ) );
 		$this->assertEquals( $expected, $this->get_results( array( $this->cat_ids, '', '', ', ', $before_last ), false ) );
 	}
 
-	function test_argument_none() {
+	public function test_argument_none() {
 		$missing = 'No categories to list.';
 		$expected = '<ul><li>' . $missing . '</li></ul>';
 		$this->assertEquals( $expected, $this->get_results( array( array(), '<ul><li>', '</li></ul>', '</li><li>', '', $missing ) ) );
@@ -132,13 +158,13 @@ class Linkify_Categories_Test extends WP_UnitTestCase {
 	/**
 	 * @expectedDeprecated linkify_categories
 	 */
-	function test_deprecated_function() {
-		$this->assertEquals( $this->expected_output( 1, $this->cat_ids[0] ), $this->get_results( array( $this->cat_ids[0] ), false, true ) );
-		$this->assertEquals( $this->expected_output( 5, $this->cat_ids[0] ), $this->get_results( array( $this->cat_ids ), false, true ) );
+	public function test_deprecated_function() {
+		$this->assertEquals( $this->expected_output( 1 ), $this->get_results( array( $this->cat_ids[0] ), false, true ) );
+		$this->assertEquals( $this->expected_output( 5 ), $this->get_results( array( $this->cat_ids ), false, true ) );
 		$cat = get_category( $this->cat_ids[0] );
-		$this->assertEquals( $this->expected_output( 1, $cat->cat_ID ), $this->get_results( array( $cat->slug ), false, true ) );
+		$this->assertEquals( $this->expected_output( 1 ), $this->get_results( array( $cat->slug ), false, true ) );
 		$cat_slugs = array_map( array( $this, 'get_slug' ), $this->cat_ids );
-		$this->assertEquals( $this->expected_output( 5, $this->cat_ids[0] ), $this->get_results( array( $cat_slugs ), false, true ) );
+		$this->assertEquals( $this->expected_output( 5 ), $this->get_results( array( $cat_slugs ), false, true ) );
 	}
 
 }

4569b49342978bbfbfe1aa9ac5180d8e505ee32e
Scott Reilly <scott@coffee2code.com>
2021-10-16 23:40:20 -0700

    Unit tests: In bootstrap, store path to plugin file constant

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -5,6 +5,8 @@
  * @package Linkify_Categories
  */
 
+define( 'LINKIFY_CATEGORIES_PLUGIN_FILE', dirname( __FILE__, 3 ) . '/linkify-categories.php' );
+
 ini_set( 'display_errors', 'on' );
 error_reporting( E_ALL );
 
@@ -20,7 +22,7 @@ require_once $_tests_dir . '/tests/phpunit/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( __FILE__, 3 ) . '/linkify-categories.php';
+	require LINKIFY_CATEGORIES_PLUGIN_FILE;
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 

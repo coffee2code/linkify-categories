97f47ab07b62a84dabe9133737b99bf15763dad1
Scott Reilly <scott@coffee2code.com>
2025-04-26 00:01:03 -0600

    Widget: Include version number in class name to ensure use of expected version

--- tests/phpunit/tests/linkify-categories.php
+++ tests/phpunit/tests/linkify-categories.php
@@ -73,7 +73,7 @@ class Linkify_Categories_Test extends WP_UnitTestCase {
 	}
 
 	public function test_widget_version() {
-		$this->assertEquals( '005', c2c_LinkifyCategoriesWidget::version() );
+		$this->assertEquals( '006', c2c_LinkifyCategoriesWidget::version() );
 	}
 
 	public function test_widget_hooks_widgets_init() {
